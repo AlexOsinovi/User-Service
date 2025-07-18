770899e5c74a6b880ab2537369369578b2b23beb
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-07-18 13:01:50 +0300

    Init commit

---
+++ src/test/java/by/osinovi/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.osinovi.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
