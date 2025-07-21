2b238a087e59317d058d5acebc771c35c44fde47
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-07-21 15:14:37 +0300

    Add: add @Param to methods with SQL and JPQL

--- src/main/java/by/osinovi/userservice/repository/CardRepository.java
+++ src/main/java/by/osinovi/userservice/repository/CardRepository.java
@@ -3,6 +3,7 @@ package by.osinovi.userservice.repository;
 import by.osinovi.userservice.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,5 @@ public interface CardRepository extends JpaRepository<Card, Integer> {
     Optional<Card> findCardById(Integer id);
 
     @Query("SELECT c FROM Card c WHERE c.id IN :ids")
-    List<Card> findCardByIdIn(List<Integer> ids);
+    List<Card> findCardByIdIn(@Param("ids") List<Integer> ids);
 }
--- src/main/java/by/osinovi/userservice/repository/UserRepository.java
+++ src/main/java/by/osinovi/userservice/repository/UserRepository.java
@@ -3,6 +3,7 @@ package by.osinovi.userservice.repository;
 import by.osinovi.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findUserByEmail(String email);
 
     @Query(value = "SELECT * FROM users WHERE id in :ids", nativeQuery = true)
-    List<User> findUserByIdIn(List<Integer> ids);
+    List<User> findUserByIdIn(@Param("ids") List<Integer> ids);
 }
