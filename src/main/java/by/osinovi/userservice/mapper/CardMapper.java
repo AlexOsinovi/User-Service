9f9afd8a42b6913c6d0cee2e3fed6822a926c127
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-07-22 16:26:56 +0300

    Fix: fix CardMapping functionality

--- src/main/java/by/osinovi/userservice/mapper/CardMapper.java
+++ src/main/java/by/osinovi/userservice/mapper/CardMapper.java
@@ -4,11 +4,13 @@ import by.osinovi.userservice.dto.CardRequestDto;
 import by.osinovi.userservice.dto.CardResponseDto;
 import by.osinovi.userservice.entity.Card;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CardMapper {
 
     Card toEntity(CardRequestDto dto);
 
+    @Mapping(target = "userId", expression = "java(entity.getUser().getId())")
     CardResponseDto toDto(Card entity);
 }
