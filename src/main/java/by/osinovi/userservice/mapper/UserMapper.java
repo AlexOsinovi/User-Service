655fd4e7ed5eefad976bb25e463acb8f26e380dd
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-07-24 18:13:53 +0300

    Fix: Fix mappers

--- src/main/java/by/osinovi/userservice/mapper/UserMapper.java
+++ src/main/java/by/osinovi/userservice/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import by.osinovi.userservice.dto.UserResponseDto;
 import by.osinovi.userservice.entity.User;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = CardMapper.class)
+@Mapper(componentModel = "spring",uses = CardMapper.class)
 public interface UserMapper {
 
     User toEntity(UserRequestDto dto);
