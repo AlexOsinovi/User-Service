0ef5ae91fa609e7f608e17a166176635997de91f
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-09-20 18:30:36 +0300

    Refactor: add some dynamic properties

--- src/test/java/by/osinovi/userservice/integration/config/BaseIntegrationTest.java
+++ src/test/java/by/osinovi/userservice/integration/config/BaseIntegrationTest.java
@@ -1,7 +1,6 @@
 package by.osinovi.userservice.integration.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -20,24 +19,28 @@ public abstract class BaseIntegrationTest {
             .withDatabaseName("user_service_test")
             .withUsername("test_user")
             .withPassword("test_password")
-            .withStartupTimeout(java.time.Duration.ofSeconds(60))
+            .withStartupTimeout(java.time.Duration.ofSeconds(150))
             .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1));
 
     @Container
     protected static final GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:7.4.2-alpine"))
             .withExposedPorts(6379)
-            .withStartupTimeout(java.time.Duration.ofSeconds(60))
+            .withStartupTimeout(java.time.Duration.ofSeconds(150))
             .waitingFor(Wait.forLogMessage(".*Ready to accept connections.*\\n", 1));
 
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.url", () -> postgres.getJdbcUrl() + "?sslmode=disable");
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
         registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
 
         registry.add("spring.data.redis.host", redis::getHost);
         registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.liquibase.enabled", () -> false);
     }
 }
